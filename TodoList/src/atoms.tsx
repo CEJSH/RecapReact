9bc8d2f3f3f136de81fb2f2d2aa2d44c3b3b7886
CEJSH <eun2763@naver.com>
2023-10-29 14:10:26 +0900

    enum

--- TodoList/src/atoms.tsx
+++ TodoList/src/atoms.tsx
@@ -1,23 +1,37 @@
 import { atom, selector } from "recoil";
 
+export enum Categories {
+    "TO_DO" = "TO_DO",
+    "DOING" = "DOING",
+    "DONE" = "DONE"
+}
+
 export interface IToDo {
     text: string;
     id: number;
-    category: "TO_DO" | "DOING" | "DONE"
+    category: Categories
 }
+
+// handle category the user is currently selecting
+
+export const categoryState = atom<Categories>({
+    key: "category",
+    default: Categories.TO_DO
+})
+
+
 export const toDoState = atom<IToDo[]>({
     key: "toDo",
     default: [],
 })
+
 //  selector를 이용하여 category별로 구분을 하고 싶다면?
 
 export const toDoSelector = selector({
     key: "toDoSelector",
     get: ({ get }) => {
-        const toDos = get(toDoState)
-        return [
-            toDos.filter(toDo => toDo.category === "TO_DO"),
-            toDos.filter(toDo => toDo.category === "DOING"),
-            toDos.filter((toDo) => toDo.category === "DONE"),]
+        const toDos = get(toDoState);
+        const category = get(categoryState);
+        return toDos.filter((toDo) => toDo.category === category)
     }
 })
