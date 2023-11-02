51759a1ac3ed5af595ba5c6572a08e83fac20b2f
CEJSH <eun2763@naver.com>
2023-11-02 13:39:19 +0900

    dd

--- TodoList/src/atoms.tsx
+++ TodoList/src/atoms.tsx
@@ -1,6 +1,27 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
-export const toDoState = atom({
+export interface ITodo {
+    id: number;
+    text: string;
+}
+interface IToDoState {
+    [key: string]: ITodo[];
+}
+export const toDoState = atom<IToDoState>({
     key: "toDo",
-    default: ["a", "b", "c", "d", "e", "f"],
+    default: {
+        "To Do": [],
+        Doing: [],
+        Done: [],
+    },
 })
+
+
+
+
+// Movement within the same board
+// 1) make a copy only of a array that happened modifying
+// 2) then we put that copy next to the older things
+
+// Movement between the different boards
+// 1) have to make copy of 2 boards
--- TodoList/src/components/DraggableCard.tsx
+++ TodoList/src/components/DraggableCard.tsx
@@ -2,27 +2,33 @@ import { Draggable } from "react-beautiful-dnd";
 import React from "react";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Card = styled.div<{ isDragging: boolean }>`
   background-color: ${props => props.theme.cardColor};
-  padding: 5px 10px;
+  padding: 10px;
   margin-bottom:5px;
   border-radius: 5px;
+  background-color: ${(props) =>
+        props.isDragging ? "#e4f2ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+        props.isDragging ? "0px 2px 5px rgba(0, 0, 0, 0.05)" : "none"};
 `;
 
 interface IDraggableCardProps {
-    toDo: string;
+    toDoId: number;
+    toDoText: string;
     index: number;
 }
-function DraggableCard({ toDo, index }: IDraggableCardProps) {
-    console.log(toDo, "has been rendered")
+function DraggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
+    console.log(toDoId, "has been rendered")
     return (
-        <Draggable draggableId={toDo} key={toDo} index={index} >
-            {(magic) => (<Card
+        <Draggable draggableId={toDoId + ""} key={toDoId} index={index} >
+            {(magic, snapshot) => (<Card
+                isDragging={snapshot.isDragging}
                 ref={magic.innerRef}
                 {...magic.draggableProps}
                 {...magic.dragHandleProps}
             >
-                {toDo}
+                {toDoText}
             </Card>)}
         </Draggable>
     )
